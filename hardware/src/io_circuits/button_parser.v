aabb739c8e6a1f18c719328725daf950ddd00f7c
Tan Nguyen <tan.nqd@gmail.com>
2020-03-21 20:18:54 -0700

    fixed button_parser

--- hardware/src/io_circuits/button_parser.v
+++ hardware/src/io_circuits/button_parser.v
@@ -1,21 +1,19 @@
-/*
-    This is a wrapper module for the synchronizer -> debouncer -> edge detector signal chain for button inputs
-*/
+// This module instantiates the synchronizer -> debouncer -> edge detector signal chain for button inputs
 module button_parser #(
-    parameter width = 1,
-    parameter sample_count_max = 25000,
-    parameter pulse_count_max = 150
+    parameter WIDTH = 1,
+    parameter SAMPLE_CNT_MAX = 25000,
+    parameter PULSE_CNT_MAX = 150
 ) (
     input clk,
-    input [width-1:0] in,
-    output [width-1:0] out
+    input [WIDTH-1:0] in,
+    output [WIDTH-1:0] out
 );
 
-    wire [width-1:0] synchronized_signals;
-    wire [width-1:0] debounced_signals;
+    wire [WIDTH-1:0] synchronized_signals;
+    wire [WIDTH-1:0] debounced_signals;
 
     synchronizer # (
-        .width(width)
+        .WIDTH(WIDTH)
     ) button_synchronizer (
         .clk(clk),
         .async_signal(in),
@@ -23,9 +21,9 @@ module button_parser #(
     );
 
     debouncer # (
-        .width(width),
-        .sample_count_max(sample_count_max),
-        .pulse_count_max(pulse_count_max)
+        .WIDTH(WIDTH),
+        .SAMPLE_CNT_MAX(SAMPLE_CNT_MAX),
+        .PULSE_CNT_MAX(PULSE_CNT_MAX)
     ) button_debouncer (
         .clk(clk),
         .glitchy_signal(synchronized_signals),
@@ -33,7 +31,7 @@ module button_parser #(
     );
 
     edge_detector # (
-        .width(width)
+        .WIDTH(WIDTH)
     ) button_edge_detector (
         .clk(clk),
         .signal_in(debounced_signals),
