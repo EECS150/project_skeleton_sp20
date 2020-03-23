3ac24c0919f1b9f48dd19b8f6c635ce06a4f22c2
Tan Nguyen <tan.nqd@gmail.com>
2020-03-23 03:34:33 -0700

    updated scripts

--- hardware/scripts/sim.tcl
+++ hardware/scripts/sim.tcl
@@ -1,5 +1,5 @@
 
-set project_name [lindex $argv 0]
+set project_name   [lindex $argv 0]
 set testbench_name [lindex $argv 1]
 
 set sources_file scripts/${project_name}.tcl
@@ -9,13 +9,18 @@ if {![file exists $sources_file]} {
     exit
 }
 
-open_project ${project_name}_proj/${project_name}_proj.xpr
+if {![file exists ${project_name}_proj/${project_name}_proj.xpr]} {
+    source scripts/build_project.tcl
+} else {
+    open_project ${project_name}_proj/${project_name}_proj.xpr
+}
 
-set sources_file scripts/${project_name}.tcl
 update_compile_order -fileset sources_1
 
 # Add simulation file
 add_files -fileset sim_1 -norecurse sim/${testbench_name}.v
+# Add memory initialization file
+add_files -norecurse ../software/assembly_tests/assembly_tests.mif
 
 set_property top ${testbench_name} [get_filesets sim_1]
 update_compile_order -fileset sim_1
